e240f36fe7ddc2ccc473ff8062e1a2da217ff10b
Darsh Shah <darshshah1008@gmail.com>
2025-11-22 17:30:45 -0500

    Add skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Skills = () => {
   const skills = {
-    Languages: ["Python", "C++", "C", "JavaScript", "TypeScript", "C#", "SQL"],
-    Frameworks: ["Flask", "Django", "FastAPI", "React.js", "Node.js", "Angular", "ASP.NET", "MongoDB", "NumPy", "TensorFlow"],
-    Tools: ["Git", "AWS", "Azure", "Docker", "Snowflake", "GraphQL", "PostgreSQL", "MySQL", "Postman", "Airflow", "Bash", "DBT", "Figma", "Jupyter Notebook"]
+    Languages: ["Python", "C++", "C", "SQL", "Ruby", "JavaScript", "TypeScript", "C#"],
+    "Frameworks/Libraries": ["Flask", "FastAPI", "Django", "Express", "Node.js", "Ruby on Rails", ".NET", "Pandas", "NumPy", "React", "Jest"],
+    Tools: ["Git", "AWS", "Azure", "Docker", "Kubernetes", "Snowflake", "PostgreSQL", "MySQL", "GraphQL", "Redis", "Airflow"]
   };
 
   return (
